c9ca28a36fd54ee75a780622ee153105f2cfdc56
Gustavo de Oliveira Bastos <gustavooliveira_bastos@hotmail.com>
2021-06-15 14:26:09 -0300

    Add project files.

---
+++ ExemploPolly.Api/Controllers/PollyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ExemploPolly.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PollyController : ControllerBase
+    {
+        [HttpGet("Numeros")]
+        public IActionResult Numeros()
+        {
+            int percent = new Random().Next(1, 100);
+            if (percent <= 50)
+            {
+                return Ok(new
+                {
+                    Numeros = new int[] { 1, 2, 3 }
+                });
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
